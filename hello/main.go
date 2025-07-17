8ea3d1f527a21f22d904c9f5a421985816f44f4e
jpinglove <jpinglove@163.com>
2025-07-17 23:19:50 +0800

    update

---
+++ hello/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+	var a, b, c, d int = 1, 3, 5, 7
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+
+}
